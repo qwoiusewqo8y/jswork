65a3b52f8947fb2a18f34b691d6c918d6893e3ba
qwoiusewqo8y <2997199040@qq.com>
2019-11-03 21:24:06 +0800

    work9

--- work9/cascade.js
+++ work9/cascade.js
@@ -1,5 +1,5 @@
-var collegesArr = ['电子与通信学院', '先进制造学院', '电气技术学院', '汽车学院', '计算机与设计学院', '外语商务学院', '经济贸易学院', '物流学院', '马克思主义学院']
-let majorsArr = [
+var collegesArr= ['电子与通信学院', '先进制造学院', '电气技术学院', '汽车学院', '计算机与设计学院', '外语商务学院', '经济贸易学院', '物流学院', '马克思主义学院']
+var majorsArr = [
     ['电子', '电信', '通信', '嵌入', '物联'],
     ['数控', '模具', '机设', '机自'],
     ['机电', '电气', '建电', '建智', '空调', '光电', '机器人'],
@@ -72,4 +72,34 @@ var classesArr = [
     ],[
         ['社工1801']
     ]
-]
+];
+
+function createOption(obj, data) {
+    for (var i in data) {
+        var op = new Option(data[i], i);
+        obj.options.add(op);
+    }
+}           
+
+var majorSelect = document.getElementById('majorSelect');
+collegeSelect.onchange = function() {
+    majorSelect.options.length = 0;
+    createOption(majorSelect, majorsArr[collegeSelect.value]);
+};
+
+var classeSelect = document.getElementById('classeSelect');
+majorSelect.onchange = function() {
+    collegeSelect.options.length = 0;
+    createOption(classeSelect,classesArr[collegeSelect.value][majorSelect.value]);
+};
+
+collegeSelect.onchange = function() {
+    majorSelect.length = 0;
+    createOption(majorSelect, majorsArr[collegeSelect.value]);
+    if (collegeSelect.value >= 0) {
+        majorSelect();
+    } else {
+        classeSelect.options.length = 0;
+    }
+};
+
