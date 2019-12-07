80bfbab0982d367edbdce04c694cbd985e51d107
qwoiusewqo8y <2997199040@qq.com>
2019-12-07 11:07:20 +0800

    work14

--- work12-1/index1.js
+++ work12-1/index1.js
@@ -1,41 +1,17 @@
-// 获取当前年份
 let year = new Date().getFullYear()
-// 调用calender函数，设置cal内容显示年历
 document.getElementById("cal").innerHTML = calendar(year)
+function setCalendar(year){
+    document.getElementById('cal').innerHTML = '<div class="box" id="cal"></div>'
 
-function setCalendar(year){// 定义年历生成函数
-        // 获取指定年份1月1日的星期数值
-        var w = new Date(y,0).getDay();
-        var html = '<div class="box">';
-        // 拼接每个月份的表格
-        for (var m = 1; m <= 12; ++m) {
-            html += '<table>';
-            html += '<tr class="title"><th colspan="7">' + y + '年' + m + '月</th></tr>';
-            html += '<tr><td>日</td><td>-</td><td>二</td><td>三</td><td>四</td><td>五</td><td>六</td></tr>';
+    var year = parseInt(year)
+    var pre = year-1
+    var next = year+1
     
-            // 获取月份m共有多少天
-            var max = new Date(y, m, 0).getDate();
-    
-            html += '<tr>';// 开始<tr>标签
-            for (var d = 1; d <= max; ++d) {
-                if (w && d == 1) {// 如果该月的第一天不是星期日，则填充空白
-                    html += '<td colspan="' + w + '"></td>';
-                }
-                html += '<td>' + d + '</td>';
-                if (w == 6 && d != max) {// 如果星期六不是该月的最后一天，则换行
-                    html += '</tr>';
-                } else if (d == max) { //该月的最后一天， 闭合<tr>标签
-                    html += '</tr>';
-                }
-                w = (w + 1 > 6) ? 0 : w + 1;      
-            }
-            html += '</table>';
-        }
-        html += '</div>';
-        return html;
-    }
-    // 设置preyear
-
-    // 设置curyear
-    // 设置nextyear
-    // 设置cal显示年历
+    document.getElementById('curyear').setAttribute('data-year', year);
+    document.getElementById('preyear').setAttribute('data-year', pre);
+    document.getElementById('nextyear').setAttribute('data-year', next);
+    document.getElementById('curyear').innerText = year
+    document.getElementById('preyear').innerText = "<<"+pre
+    document.getElementById('nextyear').innerText = next+ ">>"
+    document.getElementById("cal").innerHTML = calendar(year)
+}
